7617b81ec8e8e0069a66cd81c9b91cac8c6428aa
Joao-VPS <joaovictorpuglessabino@gmail.com>
2024-03-15 11:34:04 -0300

    Finalizado CRUD da CategoriaController

--- src/main/java/com/generation/alimentai/controller/CategoriaController.java
+++ src/main/java/com/generation/alimentai/controller/CategoriaController.java
@@ -1,18 +1,22 @@
 package com.generation.alimentai.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.alimentai.model.ModelCategoria;
 import com.generation.alimentai.repository.CategoriaRepository;
@@ -52,7 +56,22 @@ public class CategoriaController {
 	}
 	
 	@PutMapping
-	public ResponseEntity<ModelCategoria> put(@Valid @RequestBody ModelCategoria modelCategoria) {
-		
-	}
+    public ResponseEntity<ModelCategoria> put(@Valid @RequestBody ModelCategoria modelCategoria) {
+        return categoriaRepository.findById(modelCategoria.getId())
+                .map(resposta -> ResponseEntity.status(HttpStatus.CREATED)
+                        .body(categoriaRepository.save(modelCategoria)))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        Optional<ModelCategoria> modelCategoria = categoriaRepository.findById(id);
+
+        if (modelCategoria.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        categoriaRepository.deleteById(id);
+    }
 }
